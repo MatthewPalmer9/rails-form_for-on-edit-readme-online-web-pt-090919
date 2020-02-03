6015325bba845b5c47da6bf802acd41cdf4e0806
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2020-02-03 05:29:12 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :posts, only: [:index, :new, :show, :edit, :update]
+  resources :posts, only: [:index, :new, :edit, :update]
   get '/posts', to: 'posts#show'
   patch 'posts/:id', to: 'posts#update'
 end
